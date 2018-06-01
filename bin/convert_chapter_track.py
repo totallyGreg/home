902bec01367dfbde707fb5af57b9bcad632b3515
J. Greg Williams <totallyGreg@gmail.com>
2018-05-31 23:26:00 -0400

    added interval function

--- bin/convert_chapter_track.py
+++ bin/convert_chapter_track.py
@@ -1,32 +1,43 @@
 #!/usr/local/bin/python3
 
-#import math
+import datetime as dt
+
+def str_to_time(str): 
+    chapter_time = dt.timedelta(seconds=float(str))
+    return chapter_time
+
+def format_time(t):
+    '''Remove 3 digits from microseconds'''
+    s = str(t)
+    return s[:-3]
+
+def interval(last_chapter_time, chapter_time):
+    interval = dt.timedelta(minutes=10)
+    last_chapter_time 
+    difference = chapter_time - last_chapter_time
+    if difference > interval: 
+        return True
+    else:
+        return False
 
 name = input("Enter filename:")
 if len(name) < 1 : name = "Label Track.txt"
-try:
-    with open(name, "r") as infile, open ("Chapters.txt", "w") as outfile:
-        for chapter in infile:
-            chapter = chapter.rstrip()
-            words = chapter.split(None,2)
-            time = words[1]
-            time = float(time)
-            chapter_name = words[2]
-            seconds, millisecs = int(time), time-int(time)
-            #divmod(time, 1) # math.modf(time) # (0.5678000000000338, 1234.0)
-            seconds = int(seconds)
-            minutes, seconds = divmod(seconds, 60)
-            hours, minutes = divmod(minutes, 60)
-            
-            chapters = [('hours', hours), ('minutes', minutes), ('seconds', seconds),
-                        ('millisecs', millisecs), ('chapter_name', chapter_name)]
-            #time_string = ':'.join('{} {} {}'.format(hours, minutes, seconds)
-            #print hours,":",minutes,":",seconds":",chapter_name
-            seconds = seconds + millisecs
-            outfile.write('{}:{}:{:.3f} {}\n'.format(hours,minutes,round(seconds,4),chapter_name))
-except:
-    print ('File cannont be opened:', name)
-    exit()
 
+with open(name, "r") as infile, open ("Chapters.txt", "w") as outfile:
+    last_chapter = []
+    l_chapter_time = dt.timedelta(seconds=0)
+    count = 0
 
+    for chapter_line in infile:
+        chapter_line = chapter_line.rstrip()
+        chapter = chapter_line.split(None,2)
+        time_string,chapter_name  = chapter[1],chapter[2]
+        chapter_time = str_to_time(time_string)
+        is_chapter = interval(l_chapter_time,chapter_time)
+        # If chapter_time is not within 5 minutes of last one write it out
+        if is_chapter:
+            outfile.write('{} {}\n'.format(format_time(chapter_time),chapter_name))
+            last_chapter = chapter
+            l_time_string, l_chapter_name = last_chapter[1],last_chapter[2]
+            l_chapter_time = str_to_time(l_time_string)
 
