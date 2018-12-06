f9b19d21455ac3c6fa946bf2c3ce4b3329b42167
J. Greg Williams <totallyGreg@gmail.com>
2018-12-06 14:25:25 -0500

    fixed the repairTM.sh script

--- bin/repairTM.sh
+++ bin/repairTM.sh
@@ -1,14 +1,30 @@
-#!/bin/bash
+#\C!/bin/bash
+set -e
+set -x
 
 if [[ $(whoami) != 'root' ]]; then
-  echo "Must have root privileges to run" 
+  echo "Must have root privileges to run"
   exit 1
 fi
 
-read -p 'Enter Time Machine Hostname: ' HOSTNAME # freenasty.home.falcone.us
-read -p 'Enter Share: ' SHARE  # totally_reversed
-read -p 'Enter Username: ' USERNAME # username that created the share
-read -s -p 'Enter Password: ' PASSWORD
+
+## tmutil destinationinfo will provide the URL
+# Name          : totally_reversed
+# Kind          : Network
+# URL           : afp://totally@FreeNASty._afpovertcp._tcp.local./totally_reversed
+# Mount Point   : /Volumes/com.apple.TimeMachine.totally_reversed-F5ED3A5E-EB28-4B1A-9C07-B294FEBCBA2A
+# ID            : 0F1682E6-3015-4A9D-9741-32A42F1796F6
+
+URL=`tmutil destinationinfo | grep URL | cut -f 3 -d " "`
+
+if [ ${#URL} -eq 0 ]
+then
+  read -p 'Enter Time Machine Hostname: ' HOSTNAME # freenasty.home.falcone.us
+  read -p 'Enter Share: ' SHARE  # totally_reversed
+  read -p 'Enter Username: ' USERNAME # username that created the share
+  # read -s -p 'Enter Password: ' PASSWORD
+  # using mount_afp -i will prompt for password.
+fi
 
 TM_NAME=$(hostname -s | sed -e 's/-/ /g')
 MOUNT=/Volumes/TimeMachine
@@ -20,7 +36,8 @@ tmutil disable
 
 echo "Mounting volume"
 mkdir $MOUNT
-mount_afp afp://$USERNAME:$PASSWORD@$HOSTNAME/$SHARE $MOUNT
+#mount_afp afp://$USERNAME:$PASSWORD@$HOSTNAME/$SHARE $MOUNT
+mount_afp -i afp://$USERNAME@$HOSTNAME/$SHARE $MOUNT
 
 echo "Changing file and folder flags"
 chflags -R nouchg "$SPARSEBUNDLE"
@@ -40,13 +57,14 @@ sed -e '/RecoveryBackupDeclinedDate/{N;d;}'   \
     > "$PLIST"
 
 echo "Unmounting volumes"
-hdiutil detach /dev/$DISK
+hdiutil detach $DISK
 umount $MOUNT
 
 echo "Enabling Time Machine"
 tmutil enable
 
 echo "Starting backup"
+# tmutil startbackup --destination $DESTID
 tmutil startbackup
 
 exit 0
