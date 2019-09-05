35476114816bf1fdc29ab987841640242d838ed7
J. Greg Williams <totallyGreg@gmail.com>
2019-09-05 09:44:08 -0400

    added retrieve-cert.sh

---
+++ bin/retrieve-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# usage: retrieve-cert.sh remote.host.name [port]
+#
+REMHOST=$1
+REMPORT=${2:-443}
+
+openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
