6d5c0f395af1cce8b1d71e5a682c90d373df021d
J. Greg Williams <totallyGreg@gmail.com>
2019-12-29 12:18:41 -0600

    cleaned up using a linter

--- bin/uem_build_guest_ldom.sh
+++ bin/uem_build_guest_ldom.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# This script is used to create one or many Guest LDOM's 
+# This script is used to create one or many Guest LDOM's
 # If given a single argument of a hostname will step through the creation
-# If given a file containing multiple LDOM's it will check resources and 
+# If given a file containing multiple LDOM's it will check resources and
 # allocate according to the specifications
 # -OPTIONS  <hostname>  or -f <filename> -n (dry run just print available info)
 
 ## Any Errors stop the script immediately **
-set -e 
+set -e
 
-#Debugging 
-#if Debugging 
+#Debugging
+#if Debugging
 set -x
 [ ls ] && echo "ls executed successfully"
 
@@ -19,16 +19,16 @@ set -x
 # - Memory
 # - Disk /opt/vcapcoll/bin/disk_free ?
 # - current number of LDOMs
-# - Root pool created? 
+# - Root pool created?
 
 
-# Step 2 - Single LDOM Case - Gather specifications 
+# Step 2 - Single LDOM Case - Gather specifications
 
 # Step 2b - Multiple LDOM's passed through file - Read in Specifications
 
 # Step 3 - Given input determine if LDOM(s) can be created
 # if disks in root pool already total N+1 including new N don't add new drive
-# otherwise add new drive to root pool 
+# otherwise add new drive to root pool
 
 # Step 4 - Generate Display of New build(s) to verify before creation
 
@@ -38,8 +38,8 @@ set -x
 	# if ldom root disk already exists prompt to reuse
 	# if root_disk_count <= ldom_count +1 then add new disk
 # Create each data disk
-# Create guest with the values provided earlier                               
-# domain=$1                                                                   
-# num_cpu=$2                                                                  
-# num_mau=$3                                                                  
-# num_memory=$4                                                               
+# Create guest with the values provided earlier
+# domain=$1
+# num_cpu=$2
+# num_mau=$3
+# num_memory=$4
