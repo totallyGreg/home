c7f5a8dd15b4c0a6698c4f75912455ef21b5799a
J. Greg Williams <totallyGreg@gmail.com>
2021-05-11 19:42:46 -0400

    tweaks to make it faster and stronger

--- bin/totally_backed_up.sh
+++ bin/totally_backed_up.sh
@@ -6,4 +6,13 @@
 # - I should store it directly on iCloud instead of desktop? 
 # - Volume name should be changed from untitled to something more descriptive
 
-hdiutil create -ov ~/Desktop/Totally_Backed_Up.dmg -srcfolder ~/.gnupg -srcfolder ~/.ssh -srcfolder ~/Library/Colors -srcfolder ~/Library/ColorPickers -srcfolder ~/Library/Keychains -srcfolder ~/Library/Fonts -srcfolder ~/Library/FontCollections -srcfolder ~/Library/Sounds -srcfolder ~/Library/Preferences -srcfolder ~/Library/Screen\ Savers -srcfolder ~/Library/Application\ Support -srcfolder ~/Downloads
+## Options
+   # -[no]atomic     do [not] copy files to a temporary location and then rename them to their destination.  Atomic copies are the default.  Non-atomic copying may be slightly faster.out
+
+  # -volname volname
+  #           The newly-created filesystem will be named volname.  The default depends the filesystem being used; The default volume name in both HFS+ and APFS is `untitled'.
+  #           -volname is invalid and ignored when using -srcdevice.
+
+hdiutil create -volname Totally_Backed_Up -format UDSB -noatomic -skipunreadable -ov ~/Desktop/Totally_Backed_Up.sparsebundle -srcfolder ~/.gnupg -srcfolder ~/.ssh -srcfolder ~/Library/Colors -srcfolder ~/Library/ColorPickers -srcfolder ~/Library/Keychains -srcfolder ~/Library/Fonts -srcfolder ~/Library/FontCollections -srcfolder ~/Library/Sounds -srcfolder ~/Library/Preferences -srcfolder ~/Library/Screen\ Savers -srcfolder ~/Library/Application\ Support  -srcfolder ~/Downloads 
+
+# -srcfolder ~/Repositories
