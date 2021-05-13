27e81e2f5929951a6423156d75b5186c39a583eb
J. Greg Williams <totallyGreg@gmail.com>
2021-05-13 13:36:35 -0400

    bumped Brewfile and fixed totally_backed_up.sh to point to iCloud

--- bin/totally_backed_up.sh
+++ bin/totally_backed_up.sh
@@ -1,18 +1,44 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 # ToDo
 # - This should be encrypted since it contains secrets
-# - I wish i could figure out why hdiutil does not respect \ line separators
 # - I should store it directly on iCloud instead of desktop? 
 # - Volume name should be changed from untitled to something more descriptive
 
 ## Options
-   # -[no]atomic     do [not] copy files to a temporary location and then rename them to their destination.  Atomic copies are the default.  Non-atomic copying may be slightly faster.out
-
+  # -[no]atomic     do [not] copy files to a temporary location and then rename them to their destination.  Atomic copies are the default.  Non-atomic copying may be slightly faster
   # -volname volname
-  #           The newly-created filesystem will be named volname.  The default depends the filesystem being used; The default volume name in both HFS+ and APFS is `untitled'.
-  #           -volname is invalid and ignored when using -srcdevice.
+  # The newly-created filesystem will be named volname.  The default depends the filesystem being used; The default volume name in both HFS+ and APFS is `untitled'.
 
-hdiutil create -volname Totally_Backed_Up -format UDSB -noatomic -skipunreadable -ov ~/Desktop/Totally_Backed_Up.sparsebundle -srcfolder ~/.gnupg -srcfolder ~/.ssh -srcfolder ~/Library/Colors -srcfolder ~/Library/ColorPickers -srcfolder ~/Library/Keychains -srcfolder ~/Library/Fonts -srcfolder ~/Library/FontCollections -srcfolder ~/Library/Sounds -srcfolder ~/Library/Preferences -srcfolder ~/Library/Screen\ Savers -srcfolder ~/Library/Application\ Support  -srcfolder ~/Downloads 
+# This creates a sparsebundle containing all of the listed directories.
+# This failed when it went over 10GB in size and not using a sparsebundle
+# Also zsh did not allow me to use the backslash line break and thus I switched to #!/bin/bash
+
+# This fails in every way to write directly to the iCloud directory/cache
+iCloud="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
+
+# Archive=~/Desktop/Totally_Backed_Up.sparsebundle
+Archive="${iCloud}/Archives/Totally_Backed_Up.sparsebundle"
+echo "Backup will be created in '${Archive}'"
+
+hdiutil create -volname Totally_Backed_Up -format UDSB \
+  ${Archive} \
+  -verbose -noatomic -skipunreadable -spotlight -ov \
+  -srcfolder ~/.gnupg \
+  -srcfolder ~/.ssh \
+  -srcfolder ~/Downloads \
+  -srcfolder ~/Library/Application\ Support  \
+  -srcfolder ~/Library/Audio \
+  -srcfolder ~/Library/ColorPickers \
+  -srcfolder ~/Library/Colors \
+  -srcfolder ~/Library/FontCollections \
+  -srcfolder ~/Library/Fonts \
+  -srcfolder ~/Library/Keychains \
+  -srcfolder ~/Library/Preferences \
+  -srcfolder ~/Library/QuickLook \
+  -srcfolder ~/Library/Screen\ Savers \
+  -srcfolder ~/Library/Services \
+  -srcfolder ~/Library/Sounds \
+
 
 # -srcfolder ~/Repositories
