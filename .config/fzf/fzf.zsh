99447740ee4ec35338450894dad3f67788d6c3b8
J. Greg Williams <totallyGreg@gmail.com>
2021-05-19 17:12:03 -0400

    finish XDG abstraction

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -1,12 +1,12 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/totally/.local/share/nvim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/totally/.local/share/nvim/plugged/fzf/bin"
+if [[ ! "$PATH" == *$XDG_DATA_HOME/nvim/plugged/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$XDG_DATA_HOME/nvim/plugged/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/totally/.local/share/nvim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$XDG_DATA_HOME/nvim/plugged/fzf/shell/completion.zsh" 2> /dev/null
 
 # FZF Functions
 source "$XDG_CONFIG_HOME/fzf/GITheartFZF.sh"
@@ -16,7 +16,7 @@ source "$XDG_CONFIG_HOME/fzf/Azure.sh"
 
 # Key bindings
 # ------------
-source "/Users/totally/.local/share/nvim/plugged/fzf/shell/key-bindings.zsh"
+source "$XDG_DATA_HOME/nvim/plugged/fzf/shell/key-bindings.zsh"
 source "$XDG_CONFIG_HOME/fzf/key-bindings.zsh"
 
 # Customizations
