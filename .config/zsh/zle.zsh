047562f8175d9f21caa056adfdd6764e660a955c
J. Greg Williams <283704+totallyGreg@users.noreply.github.com>
2022-03-31 14:37:58 -0400

    added cursor shape change

--- .config/zsh/zle.zsh
+++ .config/zsh/zle.zsh
@@ -15,6 +15,25 @@ bindkey '\eg' _git-status
 zle -N .toggle-watch
 bindkey '\ew' .toggle-watch     # Alt-W to toggle watch or apply to previous command
 
+ # Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+_fix_cursor() {
+   echo -ne '\e[5 q'
+}
+precmd_functions+=(_fix_cursor)
+
 # Open command line in editor
 autoload -U edit-command-line
 zle -N edit-command-line
