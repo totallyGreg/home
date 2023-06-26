9cb23c3ea92a456825a2df31547d8a76a2caf50e
J. Greg Williams <283704+totallyGreg@users.noreply.github.com>
2023-06-26 17:11:14 -0400

    switch test since scutil no longer worked

--- bin/ExpressVPN.sh
+++ bin/ExpressVPN.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 express_vpn_on() {
-  scutil -t 1 -w State:/Network/ExpressVPN
+  # scutil -t 1 -w State:/Network/ExpressVPN
+  pgrep lightway >&2 /dev/null
   local result=$?
   case $result in
     0)
