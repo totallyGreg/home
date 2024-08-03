a6cfcecc9910d26dcb179f7c48ed3a854b371395
J. Greg Williams <283704+totallyGreg@users.noreply.github.com>
2024-08-03 18:14:37 -0400

    Random updates
    - switching to tmux-256colors now that MacOS includes the correct
    terminfo
    - zed settings
    - gitignore old files

--- .config/fzf/GITheartFZF.sh
+++ .config/fzf/GITheartFZF.sh
@@ -33,14 +33,14 @@ _gt() {
     --preview 'git show --color=always {}'
 }
 
-_gh() {
-  is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
-  grep -o "[a-f0-9]\{7,\}"
-}
+# _gh() {
+#   is_in_git_repo || return
+#   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+#   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+#     --header 'Press CTRL-S to toggle sort' \
+#     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+#   grep -o "[a-f0-9]\{7,\}"
+# }
 
 _gr() {
   is_in_git_repo || return
